9b7f9ddc7f522b6d4482d5e3c2a20cbdbb8da92c
Yabodo <yabodo@vivaldi.net>
2025-05-19 10:18:15 +0300

    Add Reddit tracking pixel to Nuxt configuration and include footer links for Privacy Policy and Terms of Service on the index page.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,16 @@ export default defineNuxtConfig({
   modules: [
     '@unocss/nuxt',
   ],
+  app: {
+    head: {
+      script: [
+        {
+          innerHTML: `!function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','a2_gxi44xwhmi2v');rdt('track', 'PageVisit');`,
+          hid: 'reddit-pixel',
+        }
+      ],
+    }
+  },
   unocss: {
     theme: {
       colors: {
